2fbaeef931f225b2a903e89fa8549c8f09a56599
Igor <igor.mogilnov@mail.ru>
2023-03-05 17:12:49 +0300

    Задача 2.3 решена

---
+++ Seminars/Seminar_01/Ex_002.3/Program.cs
@@ -0,0 +1,12 @@
+﻿int a = -9;
+int b = -3;
+int max = a;
+int min = b;
+
+if (b > a) {max = b; min = a;}
+{
+    Console.Write ("max = ");
+    Console.WriteLine (max);
+    Console.Write ("min = ");
+    Console.WriteLine (min);
+}
