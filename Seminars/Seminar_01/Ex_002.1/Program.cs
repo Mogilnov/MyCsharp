d38886380875b3c3a60d902572dd72c5fd1bc7df
Igor <igor.mogilnov@mail.ru>
2023-03-05 16:12:15 +0300

    Решена задача 2.1

---
+++ Seminars/Seminar_01/Ex_002.1/Program.cs
@@ -0,0 +1,12 @@
+﻿int a = 5;
+int b = 7;
+int max = a;
+int min = b;
+
+if (b > a) {max = b; min = a;}
+{
+    Console.Write ("max = ");
+    Console.WriteLine (max);
+    Console.Write ("min = ");
+    Console.WriteLine (min);
+}
