74f06291d6a76f03648ec178ec5888bff4c246e8
Igor <igor.mogilnov@mail.ru>
2023-03-09 13:26:26 +0300

    add Example 13

---
+++ Lectures/Lecture_03/Example013/Program.cs
@@ -0,0 +1,32 @@
+﻿// === Работа с текстом
+// Дан текст. В тексте все пробелы нужно заменить черточками,
+// маленькие буквы "к" заменить большими "К",
+// ф большие "С" заменить маленькими "с".
+
+// string s = "qwerty"
+//             0123
+// s[3] // r
+
+
+string text = "- Я думаю, - сказал князь, улыбаясь, - что, "
+            + "ежели бы вас послали вместо нашего милого Винценгероде,"
+            + "вы бы взяли приступом согласие прусского короля."
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = string.Empty;
+
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if(text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+
+
+    return result;
+}
+
+string newText = Replace(text, ' ', '|');
+Console.WriteLine(newText);
