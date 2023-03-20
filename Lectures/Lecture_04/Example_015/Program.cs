5979027bea7b136ffbf2106424b27d90f398a78b
Igor <igor.mogilnov@mail.ru>
2023-03-20 11:44:47 +0300

    add exapmle 15

---
+++ Lectures/Lecture_04/Example_015/Program.cs
@@ -0,0 +1,11 @@
+﻿string[,] table = new string [2,5];
+table[1,2] = "слово";
+
+for (int row=0; row<2; row++)
+{
+    for (int column=0; column<5; column++)
+    {
+        Console.Write($"-{table[row, column]}-");
+    }
+    Console.WriteLine();
+}
