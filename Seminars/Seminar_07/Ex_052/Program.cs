53664009458a1803912f706a249f7df51349ad35
Igor <igor.mogilnov@mail.ru>
2023-03-21 15:03:06 +0300

    задача 52 решена

---
+++ Seminars/Seminar_07/Ex_052/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+double [,] array = new double [,]
+{
+    {1, 4, 7, 2},
+    {5, 9, 2, 3},
+    {8, 4, 2, 4},
+};
+
+void GetAverage (double [,] arr)
+{
+    double AvCol1 = (arr[0,0]+arr[1,0]+arr[2,0])/3;
+    double a = Math.Truncate(10 * AvCol1) / 10;
+    double AvCol2 = (arr[0,1]+arr[1,1]+arr[2,1])/3;
+    double b = Math.Truncate(10 * AvCol2) / 10;
+    double AvCol3 = (arr[0,2]+arr[1,2]+arr[2,2])/3;
+    double c = Math.Truncate(10 * AvCol3) / 10;
+    double AvCol4 = (arr[0,3]+arr[1,3]+arr[2,3])/3;
+    double d = Math.Truncate(10 * AvCol4) / 10;
+    Console.WriteLine($"{a}, {b}, {c}, {d}");
+}
+GetAverage (array);
