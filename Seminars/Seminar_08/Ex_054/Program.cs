8f156e857126601a1e2c99e7a2c8867aecb7adf4
Igor <igor.mogilnov@mail.ru>
2023-03-22 17:52:06 +0300

    задача 54 решена

---
+++ Seminars/Seminar_08/Ex_054/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+int[,] array = new int[,]
+{
+    {1, 4, 7, 2},
+    {5, 9, 2, 3},
+    {8, 4, 2, 4},
+};
+
+void DescendArr(int[,] arr)
+{
+
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int k = 0; k < arr.GetLength(1)-1; k++)
+            if (array[i, k] < array[i, k + 1])
+            {
+                int temp = array[i, k+1];
+                array[i, k+1] = array[i, k];
+                array[i, k] = temp;
+            }
+        }
+    }
+
+}
+
+void PrintArr(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+DescendArr(array);
+PrintArr(array);
